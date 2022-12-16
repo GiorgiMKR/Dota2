4af733a2aaa10b88467a369c71c3f2de3bf6cdd2
Giorgi Mkrtichiani <giorgimkr@gmail.com>
2022-12-16 20:42:39 +0400

    Initial Commit

---
+++ Dota2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2
+//
+//  Created by macbook on 12/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
